5165ab6450c9c80b8a3e0f70c3a2f6f0b8567237
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-08-14 19:56:14 +0530

    adding cpp file

--- variables.cpp
+++ variables.cpp
@@ -6,5 +6,16 @@ int main(){
     short sa= 1;
     //size of the variables short<int<long<long long
     cout<<sa;
+
+
+    float score=34.54;
+    // if dont want to change the value of float,int use const
+    float const ff=44.4;
+    // ff=55.5;
+    cout<<ff;
+    double score2=45.323;
+    long double score3=45.3212;
+    cout<<score<<endl<<score2<<endl<<score3<<endl;
+     
     return 0;
 }
