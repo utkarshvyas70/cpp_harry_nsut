d6571145c1f5fb678c6791596c272211acc80d42
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-08-15 23:17:36 +0530

    adding cpp file

---
+++ userInput.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout<<"Enter First Number";
+    cin>>a;
+
+    cout<<"Enter the second number";
+    cin>>b;
+    return 0;
+    }
