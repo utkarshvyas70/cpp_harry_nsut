74484088335aa06069c260395a9c2d07378bbd23
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-08-17 00:38:31 +0530

    adding cpp file

---
+++ loops.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int index=0;
+    while (index<34)
+    {
+        cout<<"We are at index : "<<index<<endl;
+        index=index+1;
+    }
+    
+    return 0;
+}
